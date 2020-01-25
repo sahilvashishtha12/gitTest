a3533e5667c8a2a7b8a00ca4762e96655b77bd6f
sahilvashishtha12 <sahilvashishtha12@gmail.com>
2020-01-25 13:58:54 +0530

    first commit

---
+++ src/arrays/newArray.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class newArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		var a;
+
+	}
+
+}
